f2fc5230ad0b34251d14f264ae68893bddd7aed0
MohdAdil-02 <mohdadilansari02@gmail.com>
2026-07-03 01:18:04 +0530

    feat:add login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,3 +1,5 @@
+
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../model/User');
@@ -23,6 +25,40 @@ router.post('/register', async (req, res) => {
     });
 
     res.status(201).json({ message: 'User registered successfully', userId: user._id });
+
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
 
   } catch (err) {
     res.status(500).json({ message: 'Server error', error: err.message });
