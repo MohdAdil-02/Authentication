2c5839fb9e71a334405c59914657acc25354a493
MohdAdil-02 <mohdadilansari02@gmail.com>
2026-07-03 01:09:15 +0530

    feat: add resgister route

--- server/server.js
+++ server/server.js
@@ -8,9 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log('MongoDB connection error:', err));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
